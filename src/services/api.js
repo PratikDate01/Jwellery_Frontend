503ccc2f23fef9b1c91b697067264b46fa5bb603
Pratik Date <pratikdate537@gmail.com>
2026-03-06 12:03:23 +0530

    Initial frontend commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 ﻿import axios from 'axios';
 
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api/',
+  baseURL: `${API_BASE_URL}/api/`,
 });
 
 api.interceptors.request.use(
